31c307d6beb23782d5282e454a800d61c95bfd67
farid-ad <faridaboudiab123@gmail.com>
2021-10-17 12:23:20 +0300

    finish the reminder birthday

---
+++ src/Data.js
@@ -0,0 +1,32 @@
+export default [
+    {
+        id: 1,
+        name: 'Khaldoun Abou diab',
+        age: 31,
+        image: 'https://scontent.fiev25-2.fna.fbcdn.net/v/t1.18169-9/20031678_255670904918893_4553182906054451145_n.jpg?_nc_cat=111&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=IZzKewx1ZzIAX_8Q2Zj&_nc_ht=scontent.fiev25-2.fna&oh=bd4de0ed2c9ee2a57eb8d6452e53efa1&oe=61929E8A',
+    },
+    {
+        id: 2,
+        name: 'Bahaa Abou diab',
+        age: 26,
+        image: 'https://scontent.fiev25-1.fna.fbcdn.net/v/t1.6435-9/169258694_10159161603673430_8431523195690638613_n.jpg?_nc_cat=106&ccb=1-5&_nc_sid=8bfeb9&_nc_ohc=aS3EXmAVUdEAX8tg6oR&tn=bs24g8wDn8BVcx5P&_nc_ht=scontent.fiev25-1.fna&oh=53549d7408b85df6404410b538112348&oe=61922245',
+    },
+    {
+        id: 3,
+        name: 'Oussama Bou diab',
+        age: 22,
+        image: 'https://scontent.fiev25-2.fna.fbcdn.net/v/t1.18169-9/14563495_1195402080533015_7160342448253723987_n.jpg?_nc_cat=104&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=QxHzRcWmoQgAX9QFvdC&_nc_ht=scontent.fiev25-2.fna&oh=1b3ff89f7a9789a9bc090e4b4074b9ea&oe=6190C735',
+    },
+    {
+        id: 4,
+        name: 'Adonis Abou diab',
+        age: 22,
+        image: 'https://scontent.fiev25-1.fna.fbcdn.net/v/t1.6435-9/241628445_4673621445995007_1824059823153220260_n.jpg?_nc_cat=100&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=40w3WHgYg7IAX9zjWVP&_nc_ht=scontent.fiev25-1.fna&oh=2a9ee4b82ff7e0c99ab76ed0e0d3682b&oe=618F9C2C',
+    },
+    {
+        id: 5,
+        name: 'Farid Abou Diab',
+        age: 22,
+        image: 'https://scontent.fiev25-1.fna.fbcdn.net/v/t1.6435-9/s640x640/242819900_3066047867008980_4623507192511337945_n.jpg?_nc_cat=100&ccb=1-5&_nc_sid=730e14&_nc_ohc=En_K5Pljh2oAX8C5Z56&_nc_ht=scontent.fiev25-1.fna&oh=7064b9744918e8ac45e498b3a394f19d&oe=6193405C',
+    },
+]
